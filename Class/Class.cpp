6cf4373dd362684436312937f313cf5523bcf57d
4D-20\koyo <a>
2026-07-14 13:31:24 +0900

    構造体を増やす

--- Class/Class.cpp
+++ Class/Class.cpp
@@ -1,1 +1,32 @@
 ﻿#include <iostream>
+
+
+struct A
+{
+	char a;
+	int b;
+	char c;
+};
+
+struct B
+{
+	char a;
+	char b;
+	int c;
+};
+
+struct C
+{
+	int a;
+	char b;
+	char c;
+};
+int main()
+{
+	std::cout << sizeof(A) << std::endl;
+	std::cout << sizeof(B) << std::endl;
+	std::cout << sizeof(C) << std::endl;
+
+	//std::cout << alignof(A) << std::endl;
+
+}
