0ef39a81b42efa4541faef2204ee04acfba05656
4D-20\koyo <a>
2026-07-14 14:01:31 +0900

    変数の変更

--- Class/Class.cpp
+++ Class/Class.cpp
@@ -3,16 +3,26 @@
 
 struct A
 {
-	char a;
-	int b;
-	char c;
-	double d;
-	float e;
-	unsigned int f;
-	unsigned long g;
-	unsigned char h;
-	unsigned short i;
-	unsigned long long j;
+	//char a;
+	//int b;
+	//char c;
+	//double d;
+	//float e;
+	//unsigned int f;
+	//unsigned long g;
+	//unsigned char h;
+	//unsigned short i;
+	//unsigned long long j;
+
+	int8_t a;
+	int32_t b;
+	int8_t c;
+	float d;
+	double e;
+	uint32_t f;
+	uint64_t g;
+	uint16_t h;
+	uint8_t i;
 };
 int main()
 {
